8be5fe83debbe9b753b2d17b472c53290a82e23a
Gus Liakos <Gliakos@cnm.edu>
2018-04-19 00:54:26 -0600

    created outline of html base and section one

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<head>
+
+</head>
+<body>
+		<section id="purpose-audience-goal">
+			<h1>Persona, Audience, Goal</h1>
+			<h2>Purpose:</h2>
+			<p>: This project will be a professional portfolio website with a focus on front-end web development.</p>
+			<h2>Audience:</h2>
+			<p></p>
+			<h2>Goal:</h2>
+	<h1>
+
+
+<!--		Audience: This site is designed with a professional audience in mind, and is targeted to individuals within the web development and I.T. community nationwide. Target audience to include: startup founders, hiring managers, recruiters, and peers.-->
+<!---->
+<!--		Goal: The primary goal of this site is for users of the site to initiate contact with me in regards to employment and collaboration opportunities through the contact form, and/or the connected social media profiles.-->
+<!--		The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence.-->
+	</h1>
+	</section>
+</body>
