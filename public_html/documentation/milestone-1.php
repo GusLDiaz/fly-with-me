14f5cff11a32194ba8deeac3f9452a9f1884a159
Gus Liakos <Gliakos@cnm.edu>
2018-04-19 01:08:44 -0600

    section 2 outlining and headers

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,20 +4,47 @@
 
 </head>
 <body>
-		<section id="purpose-audience-goal">
-			<h1>Persona, Audience, Goal</h1>
-			<h2>Purpose:</h2>
-			<p>: This project will be a professional portfolio website with a focus on front-end web development.</p>
-			<h2>Audience:</h2>
-			<p></p>
-			<h2>Goal:</h2>
-	<h1>
-
-
-<!--		Audience: This site is designed with a professional audience in mind, and is targeted to individuals within the web development and I.T. community nationwide. Target audience to include: startup founders, hiring managers, recruiters, and peers.-->
-<!---->
-<!--		Goal: The primary goal of this site is for users of the site to initiate contact with me in regards to employment and collaboration opportunities through the contact form, and/or the connected social media profiles.-->
-<!--		The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence.-->
-	</h1>
+	<section id="purpose-audience-goal">
+		<h1>Persona, Audience, Goal</h1>
+		<h2>Purpose:</h2>
+		<p>: This project will be a professional portfolio website with a focus on front-end web development.</p>
+		<h2>Audience:</h2>
+		<p></p>
+		<h2>Goal:</h2>
+		<p></p>
 	</section>
+	<section id="Persona">
+		<h1>Persona</h1>
+		<h2>Name: <strong></strong></h2>
+		<h2>Age:</h2>
+		<h2>Gender: Female</h2>
+	<div>
+		<h2>Profession:</h2>
+		<p></p>
+	</div><div>
+		<h2>Technology:</h2>
+			<p></p>
+		</div><div>
+			<h2>Outlook</h2>
+		</div><div>
+			<h2>Frustrations/Needs</h2>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div><div>
+			<h2>Goals</h2>
+			<p></p>
+		</div><div>
+			<h2>User Story</h2>
+			<p></p>
+			</div>
+	</section>
+
+
+	<!--		Audience: This site is designed with a professional audience in mind, and is targeted to individuals within the web development and I.T. community nationwide. Target audience to include: startup founders, hiring managers, recruiters, and peers.-->
+	<!---->
+	<!--		Goal: The primary goal of this site is for users of the site to initiate contact with me in regards to employment and collaboration opportunities through the contact form, and/or the connected social media profiles.-->
+	<!--		The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence.-->
 </body>
